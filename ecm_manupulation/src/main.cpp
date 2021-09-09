8793a8bd0a96b48933ee5e83425ed0ba07ddaee5
Shreyas Chandra Sekhar <cshreyastech@gmail.com>
2021-09-09 15:44:20 -0700

    Fixed Ambf client singleton useage

--- ecm_manupulation/src/main.cpp
+++ ecm_manupulation/src/main.cpp
@@ -6,7 +6,10 @@ int main(int argc, char* argv[])
 {
     //Kinematics kinematics;
 //    kinematics.AMBFIK();
-    AMBFClient ambfclient = new AMBFClient();
-    ClientPtr client = ambfclient.getInstance();
+
+    AMBFClient *client;
+    ClientPtr ambfClient = client->getInstance();
+    ambfClient->connect();
+    ambfClient->cleanUp();
     return 0;
 }

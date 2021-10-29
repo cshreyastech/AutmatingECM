21f763be45e5004852cef6280ccd265412fc8e7b
cshreyastech <cshreyastech@gmail.com>
2021-10-28 18:29:31 -0700

    Fixed build issues

--- cmake/ambf_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(ambf_msgs_MSG_INCLUDE_DIRS "/mnt/OneTB/localcodebase/ambf_repos/aimlab/ambf/ambf_ros_modules/ambf_msgs/msg")
-set(ambf_msgs_MSG_DEPENDENCIES geometry_msgs;std_msgs)
--- ecm_manupulation/src/main.cpp
+++ ecm_manupulation/src/main.cpp
@@ -1,15 +1,18 @@
 //#include "ecm_manupulation/Dynamics.h"
 //#include "ecm_manupulation/Kinematics.h"
 #include "ecm_manupulation/AMBFClient.h"
+//#include "rbdl_model/BuildRBDLModel.h"
 
 int main(int argc, char* argv[])
 {
+    const string actuator_config_file = "/home/nathanielgoldfarb/catkin_ws/src/ambf_controller_testing/model/ecm/default.yaml";
+    
     //Kinematics kinematics;
 //    kinematics.AMBFIK();
 
-    AMBFClient *client;
-    ClientPtr ambfClient = client->getInstance();
-    ambfClient->connect();
-    ambfClient->cleanUp();
+    // AMBFClient *client;
+    // ClientPtr ambfClient = client->getInstance();
+    // ambfClient->connect();
+    // ambfClient->cleanUp();
     return 0;
 }
